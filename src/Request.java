937c32f7b5903869e595e8e2b343d0e6435636d6
Nat Nats <vargasnatnat10@gmail.com>
2025-10-08 14:27:37 +0800

    UserManager done User done RequestManager on going Request blueprint done

--- src/Request.java
+++ src/Request.java
@@ -1,10 +1,21 @@
 public class Request {
     private String staffRequest;
+    private int requestId;
 
-    Request(String staffRequest) {
+    Request(String staffRequest, int requestId) {
         this.staffRequest = staffRequest;
+        this.requestId = requestId;
+
     }
 
     public String getStaffRequest() { return staffRequest; }
+    public int getRequestId() { return requestId; }
     public void setStaffRequest(String staffRequest) { this.staffRequest = staffRequest; }
+    public void setRequestId(String requestId ) { this.requestId = requestId; }
+
+    @Override
+    public String toString() {
+        return "Request ID: " + requestId;
+    }
+
 }
--- src/staffRequestManager.java
+++ src/staffRequestManager.java
@@ -1,2 +1,7 @@
+import java.util.ArrayList;
+
 public class staffRequestManager {
+    private ArrayList<Request> requests = new ArrayList<>();
+
+
 }
